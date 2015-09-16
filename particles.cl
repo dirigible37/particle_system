3c22eb9a5c559057264616510aca04fe50b1f5d8
Winslow B Mohr <wmohr@imp10.cs.clemson.edu>
2015-09-16 18:00:24 -0400

    Added basic particle system

---
+++ particles.cl
@@ -0,0 +1,83 @@
+
+#define STEPS_PER_RENDER 30
+#define MASS 1.0f
+#define DELTA_T (0.002f)
+#define FRICTION 0.4f
+#define RESTITUTION 0.7f
+
+#define EPS_DOWN (-0.2f) // gravity
+#define V_DRAG (4.0f)
+
+//
+// Circle is x^2 + z^2 - r^2 = 0, and so normal is (2x,2z) and
+// tangent is (-2z,2x).  Subtract (x,z) so that net force in 
+// the normal dir (dot (2x,2z)) is negative ... then add drag.
+//
+float4 getforce(float4 pos, float4 vel)
+{
+	float4 force;
+
+	force.x = (-2.0f*pos.z - pos.x) - V_DRAG*vel.x;
+	force.y = EPS_DOWN -V_DRAG*vel.y;
+	force.z = (2.0f*pos.x - pos.z) -V_DRAG*vel.z;
+	force.w = 1.0f;
+	return(force);
+}
+
+#define MULT (87.0f)
+#define MOD (3647.0f)
+
+float goober(float prev)
+{
+	prev *= (MOD*MULT);
+	return(fmod(prev,MOD)/MOD);
+}
+
+__kernel void VVerlet(__global float4* p, __global float4* v, __global float* r)
+{
+	unsigned int i = get_global_id(0);
+	float4 force, zoom;
+	float radius, mylength;
+
+	for(int steps=0;steps<STEPS_PER_RENDER;steps++){
+		force = getforce(p[i],v[i]);
+		v[i] += force*DELTA_T/2.0;
+		p[i] += v[i]*DELTA_T;
+		force = getforce(p[i],v[i]);
+		v[i] += force*DELTA_T/2.0;
+
+		radius = sqrt(p[i].x*p[i].x + p[i].z*p[i].z);
+		if((radius< 0.05f)||(p[i].y<0.0f)){
+			// regenerate position and velocity
+			zoom.x = r[i]+0.2f;
+			r[i] = goober(r[i]);
+			zoom.y = 0.2f*r[i]+0.8f;
+			r[i] = goober(r[i]);
+			zoom.z = 2.0f*(r[i]-0.5f);
+			p[i] = zoom;
+			v[i] = (float4)(0.0f,0.0f,0.0f,1.0f);
+			r[i] = goober(r[i]);
+		}
+		else{
+			// Check for wall collision.  Usually it's (p - q) o n < 0, 
+			// where p is the point in question and q is a point in
+			// the plane with normal n; but here q = (-.5,0,0) 
+			// and n = (1,0,0), so it's simply p.x < -.5.
+			if(p[i].x<-0.5){
+				// Bounce the point.  Usually it's 
+				// vout = vin - (1+r)(vin o n)n
+				//      - f*(vin - (vin o n)n)/||vin - (vin o n)n||
+				// but here (vin o n)n) = (v[i].x,0,0), and so
+				// vin - (vin o n)n = (0,v[i].y,v[i].z).
+				p[i].x = -0.49;
+				v[i].x = v[i].x - ((1.0 + RESTITUTION)*v[i].x);
+				mylength = sqrt(v[i].y*v[i].y + v[i].z*v[i].z);
+				if(mylength>0.0){
+					v[i].y -= FRICTION*v[i].y/mylength;
+					v[i].z -= FRICTION*v[i].z/mylength;
+				}
+			}
+		}
+	}
+	p[i].w = 1.0f;
+}
---
+++ particles.cpp
@@ -0,0 +1,325 @@
+
+// This particle sytem with collisions uses velocity Verlet integration.
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <GL/glx.h>
+#include <CL/cl.h>
+#include <CL/cl_gl.h>
+#include <CL/cl_ext.h>
+#include <CL/cl_gl_ext.h>
+#include <CL/cl_platform.h>
+#include <CL/opencl.h>
+#include "RGU.h"
+
+#define YAXIS 1
+#define BOX 2
+#define WALL 3
+#define BORDER 4
+
+GLuint OGL_VBO = 1;
+#define NUMBER_OF_PARTICLES 512*512
+#define DATA_SIZE (NUMBER_OF_PARTICLES*4*sizeof(float)) 
+
+cl_context mycontext;
+cl_command_queue mycommandqueue;
+cl_kernel mykernel;
+cl_program myprogram;
+cl_mem oclvbo, dev_velocity, dev_rseed;
+size_t worksize[] = {NUMBER_OF_PARTICLES}; 
+size_t lws[] = {128}; 
+
+float host_position[NUMBER_OF_PARTICLES][4];
+float host_velocity[NUMBER_OF_PARTICLES][4];
+float host_rseed[NUMBER_OF_PARTICLES];
+
+void do_kernel()
+{
+	cl_event waitlist[1];
+
+	clEnqueueNDRangeKernel(mycommandqueue,mykernel,1,NULL,worksize,lws,0,0,
+			&waitlist[0]);
+	clWaitForEvents(1,waitlist);
+}
+
+void do_material()
+{
+	float mat_ambient[] = {0.0,0.0,0.0,1.0};
+	float mat_diffuse[] = {0.0,0.4,0.2,1.0};
+	float mat_specular[] = {1.0,1.0,1.0,1.0};
+	float mat_shininess[] = {2.0};
+
+	glMaterialfv(GL_FRONT,GL_AMBIENT,mat_ambient);
+	glMaterialfv(GL_FRONT,GL_DIFFUSE,mat_diffuse);
+	glMaterialfv(GL_FRONT,GL_SPECULAR,mat_specular);
+	glMaterialfv(GL_FRONT,GL_SHININESS,mat_shininess);
+}
+
+void do_material_points()
+{
+	float mat_ambient[] = {0.0,0.0,0.0,1.0};
+	float mat_diffuse[] = {1.0,1.0,0.1,1.0};
+	float mat_specular[] = {1.0,1.0,1.0,1.0};
+	float mat_shininess[] = {2.0};
+
+	glMaterialfv(GL_FRONT,GL_AMBIENT,mat_ambient);
+	glMaterialfv(GL_FRONT,GL_DIFFUSE,mat_diffuse);
+	glMaterialfv(GL_FRONT,GL_SPECULAR,mat_specular);
+	glMaterialfv(GL_FRONT,GL_SHININESS,mat_shininess);
+}
+
+void mydisplayfunc()
+{
+	void *ptr;
+	glFinish();
+	clEnqueueAcquireGLObjects(mycommandqueue,1,&oclvbo,0,0,0);
+	do_kernel();
+	clEnqueueReleaseGLObjects(mycommandqueue, 1, &oclvbo, 0,0,0);
+	clFinish(mycommandqueue);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+	do_material();
+	glCallList(WALL);
+
+	// Frame is drawn to blend with background.
+	glEnable(GL_BLEND);
+	glDisable(GL_LIGHTING);
+	glCallList(YAXIS);
+	glCallList(BOX);
+	glPushMatrix();
+	glTranslatef(-1.0,0.0,0.0);
+	glCallList(BOX);
+	glTranslatef(0.0,0.0,-1.0);
+	glCallList(BOX);
+	glTranslatef(1.0,0.0,0.0);
+	glCallList(BOX);
+	glPopMatrix();
+	glDisable(GL_DEPTH_TEST);
+	glCallList(BORDER);
+	glEnable(GL_DEPTH_TEST);
+
+	glDisable(GL_BLEND);
+	glEnable(GL_LIGHTING);
+	do_material_points();
+	glBindBuffer(GL_ARRAY_BUFFER,OGL_VBO);
+	glVertexPointer(4,GL_FLOAT,0,0);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glDrawArrays(GL_POINTS, 0, NUMBER_OF_PARTICLES);
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glutSwapBuffers();
+	glutPostRedisplay();
+}
+
+void setup_the_viewvol()
+{
+	float eye[] = {2.5, 1.8, 2.0};
+	float view[] = {0.0, 0.0, 0.0};
+	float up[] = {0.0, 1.0, 0.0};
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0,1.0,0.1,20.0);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(eye[0],eye[1],eye[2],view[0],view[1],view[2],up[0],up[1],up[2]);
+}
+
+void do_lights()
+{
+	float light_ambient[] = { 0.0, 0.0, 0.0, 0.0 };
+	float light_diffuse[] = { 0.8, 0.8, 0.8, 0.0 };
+	float light_specular[] = { 1.0, 1.0, 1.0, 0.0 };
+	float light_position[] = { 2.0, 2.0, 2.0, 1.0 };
+	float light_direction[] = { -1.0, -1.0, -1.0, 1.0};
+
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT,light_ambient);
+	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER,1);
+
+	glLightfv(GL_LIGHT0,GL_AMBIENT,light_ambient);
+	glLightfv(GL_LIGHT0,GL_DIFFUSE,light_diffuse);
+	glLightfv(GL_LIGHT0,GL_SPECULAR,light_specular);
+	glLightf(GL_LIGHT0,GL_SPOT_EXPONENT,1.0);
+	glLightf(GL_LIGHT0,GL_SPOT_CUTOFF,180.0);
+	glLightf(GL_LIGHT0,GL_CONSTANT_ATTENUATION,0.5);
+	glLightf(GL_LIGHT0,GL_LINEAR_ATTENUATION,0.1);
+	glLightf(GL_LIGHT0,GL_QUADRATIC_ATTENUATION,0.01);
+	glLightfv(GL_LIGHT0,GL_POSITION,light_position);
+	glLightfv(GL_LIGHT0,GL_SPOT_DIRECTION,light_direction);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+}
+
+void build_call_lists()
+{
+
+	glNewList(YAXIS,GL_COMPILE);
+	glBegin(GL_LINES);
+	glColor4f(1.0,1.0,1.0,0.9);
+	glVertex3f(0.0,0.0,0.0);
+	glVertex3f(0.0,1.0,0.0);
+	glEnd();
+	glEndList();
+	glNewList(BOX,GL_COMPILE);
+	glBegin(GL_LINES);
+	glColor4f(1.0,1.0,1.0,0.9);
+	glVertex3f(0.0,0.0,0.0);
+	glVertex3f(1.0,0.0,0.0);
+	glVertex3f(0.0,0.0,0.0);
+	glVertex3f(0.0,0.0,1.0);
+	// Complete box in y=0 plane. 
+	glVertex3f(0.0,0.0,1.0);
+	glVertex3f(1.0,0.0,1.0);
+	glVertex3f(1.0,0.0,0.0);
+	glVertex3f(1.0,0.0,1.0);
+	glEnd();
+	glEndList();
+	glNewList(WALL,GL_COMPILE);
+	glBegin(GL_QUADS);
+	glNormal3f(1.0,0.0,0.0);
+	glVertex3f(-0.5,0.0,1.0);
+	glVertex3f(-0.5,1.0,1.0);
+	glVertex3f(-0.5,1.0,-1.0);
+	glVertex3f(-0.5,0.0,-1.0);
+	glEnd();
+	glEndList();
+	glNewList(BORDER,GL_COMPILE);
+	glBegin(GL_LINES);
+	glColor4f(1.0,1.0,1.0,0.8);
+	glVertex3f(-0.5,0.0,-1.0);
+	glVertex3f(-0.5,1.0,-1.0);
+	glVertex3f(-0.5,1.0,-1.0);
+	glVertex3f(-0.5,1.0,1.0);
+	glVertex3f(-0.5,1.0,1.0);
+	glVertex3f(-0.5,0.0,1.0);
+	glEnd();
+	glEndList();
+}
+
+void InitGL(int argc, char** argv)
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_RGBA|GLUT_DEPTH|GLUT_DOUBLE);
+	glutInitWindowSize(768,768);
+	glutInitWindowPosition(100,50);
+	glutCreateWindow("my_cool_cube");
+	setup_the_viewvol();
+	do_lights();
+	glPointSize(1.0);
+	glLineWidth(3.0);
+	glEnable(GL_LINE_SMOOTH);
+	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(0.1,0.2,0.35,1.0);
+	build_call_lists();
+	glewInit();
+	return;
+}
+
+double genrand()
+{
+	return(((double)(random()+1))/2147483649.);
+}
+
+void init_particles()
+{
+	int i, j;
+	for(i=0;i<NUMBER_OF_PARTICLES;i++){
+		host_position[i][0] = genrand();
+		host_position[i][1] = 0.2*genrand()+0.8;
+		host_position[i][2] = 2.0*(genrand()-0.5);
+		host_position[i][3] = 1.0;
+		for(j=0;j<4;j++) host_velocity[i][j] = 0.0;
+		host_rseed[i] = genrand();
+	}
+}
+
+void InitCL()
+{
+	cl_platform_id myplatform;
+	cl_device_id *mydevice;
+	cl_int err;
+	char* oclsource; 
+	size_t program_length;
+	unsigned int gpudevcount;
+
+	err = RGUGetPlatformID(&myplatform);
+
+	err = clGetDeviceIDs(myplatform,CL_DEVICE_TYPE_GPU,0,NULL,&gpudevcount);
+	mydevice = new cl_device_id[gpudevcount];
+	err = clGetDeviceIDs(myplatform,CL_DEVICE_TYPE_GPU,gpudevcount,mydevice,NULL);
+
+	// You need all these to get full interoperability with OpenGL:
+	cl_context_properties props[] = {
+		CL_GL_CONTEXT_KHR, (cl_context_properties)glXGetCurrentContext(),
+		CL_GLX_DISPLAY_KHR, (cl_context_properties)glXGetCurrentDisplay(),
+		CL_CONTEXT_PLATFORM, (cl_context_properties)myplatform,
+		0};
+
+	mycontext = clCreateContext(props,1,&mydevice[0],NULL,NULL,&err);
+	mycommandqueue = clCreateCommandQueue(mycontext,mydevice[0],0,&err);
+
+	oclsource = RGULoadProgSource("mp5vv.cl", "", &program_length);
+	myprogram = clCreateProgramWithSource(mycontext,1,(const char **)&oclsource,
+			&program_length, &err);
+	if(err==CL_SUCCESS) fprintf(stderr,"create ok\n");
+	else fprintf(stderr,"create err %d\n",err);
+	clBuildProgram(myprogram, 0, NULL, NULL, NULL, NULL);
+	mykernel = clCreateKernel(myprogram, "VVerlet", &err);
+	if(err==CL_SUCCESS) fprintf(stderr,"build ok\n");
+	else fprintf(stderr,"build err %d\n",err);
+
+	glBindBuffer(GL_ARRAY_BUFFER, OGL_VBO);
+	glBufferData(GL_ARRAY_BUFFER, DATA_SIZE, &host_position[0][0], GL_DYNAMIC_DRAW);
+	oclvbo = clCreateFromGLBuffer(mycontext,CL_MEM_WRITE_ONLY,OGL_VBO,&err);
+
+	dev_velocity = clCreateBuffer(mycontext,CL_MEM_READ_WRITE|CL_MEM_COPY_HOST_PTR,
+			DATA_SIZE,&host_velocity[0][0],&err); 
+
+	dev_rseed = clCreateBuffer(mycontext,CL_MEM_READ_WRITE|CL_MEM_COPY_HOST_PTR,
+			NUMBER_OF_PARTICLES*sizeof(float),&host_rseed[0],&err); 
+
+	clSetKernelArg(mykernel,0,sizeof(cl_mem),(void *)&oclvbo);
+	clSetKernelArg(mykernel,1,sizeof(cl_mem),(void *)&dev_velocity);
+	clSetKernelArg(mykernel,2,sizeof(cl_mem),(void *)&dev_rseed);
+}
+
+void cleanup()
+{
+	clReleaseKernel(mykernel);
+	clReleaseProgram(myprogram);
+	clReleaseCommandQueue(mycommandqueue);
+	glBindBuffer(GL_ARRAY_BUFFER,OGL_VBO);
+	glDeleteBuffers(1,&OGL_VBO);
+	clReleaseMemObject(oclvbo);
+	clReleaseMemObject(dev_velocity);
+	clReleaseMemObject(dev_rseed);
+	clReleaseContext(mycontext);
+	exit(0);
+}
+
+void getout(unsigned char key, int x, int y)
+{
+	switch(key) {
+		case 'q':
+			cleanup();
+		default:
+			break;
+	}
+}
+
+int main(int argc,char **argv)
+{
+	srandom(123456789);
+	init_particles();
+	InitGL(argc, argv); 
+	InitCL(); 
+	glutDisplayFunc(mydisplayfunc);
+	glutKeyboardFunc(getout);
+	glutMainLoop();
+}
